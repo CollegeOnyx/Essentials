d3a02045da9c630646176611e656997722cbcbab
CollegeOnyx <collegeonyx@gmail.com>
2026-04-13 17:31:31 -0600

    Bug Fixes, level transition

---
+++ DAGV1700/AdventureGame/Assets/My Scripts/levelEnd.cs
@@ -0,0 +1,35 @@
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+// public class levelEnd : MonoBehaviour
+// {
+
+// public Animator transition;
+
+// public float transitionTime = 1f;
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         //transform.Translate(transform.forward * Time.deltaTime * 2f);
+//     }
+
+//     private void OnTriggerEnter(Collider other)
+//     {
+       
+//         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+       
+//     }
+
+//     //IEnumerator LoadLevel(int levelIndex)
+//     //{
+//        // transition.SetTrigger("Start");
+//        // yield return new WaitForSeconds(transitionTime);
+//        // SceneManager.LoadScene(levelIndex);
+//     //}
+    
+// }
