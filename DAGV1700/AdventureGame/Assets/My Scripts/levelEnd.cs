6fbd6d5d58021769daf8559d8376240a001ea596
CollegeOnyx <collegeonyx@gmail.com>
2026-04-20 17:32:01 -0600

    Bug fix, level transition, death

--- DAGV1700/AdventureGame/Assets/My Scripts/levelEnd.cs
+++ DAGV1700/AdventureGame/Assets/My Scripts/levelEnd.cs
@@ -1,35 +1,41 @@
-// using UnityEngine;
-// using UnityEngine.SceneManagement;
-// public class levelEnd : MonoBehaviour
-// {
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class levelEnd : MonoBehaviour
+{
 
-// public Animator transition;
+public Animator transition;
+public int score;
+public int scoreWin;
 
-// public float transitionTime = 1f;
-//     // Start is called once before the first execution of Update after the MonoBehaviour is created
-//     void Start()
-//     {
-        
-//     }
+void Start()
+    {
+       if (score==scoreWin) 
+       {
+               
 
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         //transform.Translate(transform.forward * Time.deltaTime * 2f);
-//     }
+       }
+    }
 
-//     private void OnTriggerEnter(Collider other)
-//     {
-       
-//         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
-       
-//     }
+    void Update()
+    {
 
-//     //IEnumerator LoadLevel(int levelIndex)
-//     //{
-//        // transition.SetTrigger("Start");
-//        // yield return new WaitForSeconds(transitionTime);
-//        // SceneManager.LoadScene(levelIndex);
-//     //}
+    }
+
+public float transitionTime = 1f;
     
-// }
+    private void OnTriggerEnter(Collider other)
+    {
+       SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1));
+        //StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    // IEnumerator LoadLevel(int levelIndex)
+    // {
+    //    transition.SetTrigger("Start");
+    //    yield return new WaitForSeconds(transitionTime);
+    //    SceneManager.LoadScene(levelIndex);
+    // }
+    
+}
